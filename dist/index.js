103e4e262bbacb9fbed0c1f45a097d5c5a89bc79
Juliettedqb <juliettedqb@gmail.com>
2023-05-23 17:02:44 +0200

    connect to db

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose = require('mongoose');
 const app = (0, express_1.default)();
 const port = 3000;
+mongoose
+    .connect("mongodb+srv://juliettedqb:waverider_adatech@cluster0.psamih5.mongodb.net/waverider?retryWrites=true&w=majority")
+    .then((res) => {
+    console.log("Mongoose connected");
+})
+    .catch((err) => {
+    console.error(err);
+});
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server is now running yay!');
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
 import express, { Express, Request, Response } from 'express';
+
+const mongoose = require('mongoose');
 const app: Express = express();
 const port = 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://juliettedqb:*******@cluster0.psamih5.mongodb.net/waverider?retryWrites=true&w=majority"
+  )
+  .then((res: Response) => {
+    console.log("Mongoose connected");
+  })
+  .catch((err: Request) => {
+    console.error(err);
+  });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is now running yay!');
